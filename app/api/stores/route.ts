a488a85de676d8750198f18a90b58f36f778a98e
ricardomadeira93 <ricardomadeira1993@gmail.com>
2023-08-22 16:11:49 +0100

    Api Route

---
+++ app/api/stores/route.ts
@@ -0,0 +1,39 @@
+import { NextResponse } from 'next/server';
+import { auth } from '@clerk/nextjs';
+import prismadb from '@/lib/prismadb';
+
+export async function POST(req: Request) {
+  try {
+    const { userId } = auth();
+    const body = await req.json();
+
+    // check if we have a name
+    const { name } = body;
+
+    if (!userId) {
+      return new NextResponse('Unauthorized', {
+        status: 401,
+      });
+    }
+
+    if (!name) {
+      return new NextResponse('Missing name', {
+        status: 400,
+      });
+    }
+
+    const store = await prismadb.store.create({
+      data: {
+        name,
+        userId,
+      },
+    });
+
+    return NextResponse.json(store);
+  } catch (error) {
+    console.log('[STORES_POST]', error);
+    return new NextResponse('Internal Error', {
+      status: 500,
+    });
+  }
+}
